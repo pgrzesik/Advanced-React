773208adc0e01e12698ccb2933b806eebea03d7b
Piotr Grzesik <pj.grzesik@gmail.com>
2018-10-23 00:13:17 +0200

    Add example of styled components

--- sick-fits/frontend/pages/Page.js
+++ sick-fits/frontend/pages/Page.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import Header from '../components/Header';
-import Meta from '../components/Meta';
+import React from "react";
+import Header from "../components/Header";
+import Meta from "../components/Meta";
+import styled from "styled-components";
+
+const MyButton = styled.button`
+  background: red;
+  font-size: 100px;
+  span {
+    font-size: 200px;
+  }
+`;
 
 export default class Page extends React.Component {
-  render () {
+  render() {
     return (
       <div>
         <Meta />
         <Header />
+        <MyButton>
+          Click me
+          <span>Something</span>
+        </MyButton>
         {this.props.children}
       </div>
-    )
+    );
   }
 }
