a954e299b08b43736a02fa5845c8081d25989a0f
Piotr Grzesik <pj.grzesik@gmail.com>
2018-10-29 17:48:00 +0100

    Fix header link

--- sick-fits/frontend/components/Header.jsx
+++ sick-fits/frontend/components/Header.jsx
@@ -45,7 +45,9 @@ const Header = () => (
   <StyledHeader>
     <div className="bar">
       <Logo>
-        <a href="/">Sick Fits</a>
+        <Link href="/">
+          <a>Sick Fits</a>
+        </Link>
       </Logo>
       <Nav />
     </div>
