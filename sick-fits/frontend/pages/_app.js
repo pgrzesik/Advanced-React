289126fdf45d905d8e8732c0213b5d70baa23c62
Piotr Grzesik <pj.grzesik@gmail.com>
2018-11-03 22:29:46 +0100

    Setup apollo client

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,31 @@
-import App, { Container } from 'next/app';
-import Page from './Page';
+import App, { Container } from "next/app";
+import Page from "./Page";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
 
 class MyApp extends App {
-  render () {
-    const { Component } = this.props;
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    pageProps.query = ctx.query;
+    return pageProps;
+  }
+
+  render() {
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component />
+          </Page>
+        </ApolloProvider>
       </Container>
-    )
+    );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
