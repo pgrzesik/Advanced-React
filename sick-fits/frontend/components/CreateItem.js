4f9ce83256377286a89dd1b216115494dbb280e7
Piotr Grzesik <pj.grzesik@gmail.com>
2018-11-07 00:07:59 +0100

    Build basic version of sell form

---
+++ sick-fits/frontend/components/CreateItem.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Mutation } from "react-apollo";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import gql from "graphql-tag";
+
+export default class CreateItem extends React.Component {
+  state = {
+    title: "",
+    description: "",
+    largeImage: "",
+    image: "",
+    price: 0
+  };
+
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
+  render() {
+    return (
+      <Form
+        onSubmit={e => {
+          e.preventDefault();
+          console.log(this.state);
+        }}
+      >
+        <fieldset>
+          <label htmlFor="title">
+            Title
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
+              required
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="price">
+            Price
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Price"
+              required
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="description">
+            Description
+            <textarea
+              type="text"
+              id="description"
+              name="description"
+              placeholder="Description"
+              required
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </fieldset>
+      </Form>
+    );
+  }
+}
