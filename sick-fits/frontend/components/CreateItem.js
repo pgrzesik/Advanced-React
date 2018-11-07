486d9cdf6d76a9268e7060560512594f7b505910
Piotr Grzesik <pj.grzesik@gmail.com>
2018-11-08 00:46:22 +0100

    Add uploading images

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import { Mutation } from "react-apollo";
-import Form from "./styles/Form";
-import ErrorMessage from "./ErrorMessage";
-import formatMoney from "../lib/formatMoney";
 import gql from "graphql-tag";
 import Router from "next/router";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -26,85 +26,125 @@ const CREATE_ITEM_MUTATION = gql`
   }
 `;
 
-export default class CreateItem extends React.Component {
+class CreateItem extends Component {
   state = {
-    title: "",
-    description: "",
-    largeImage: "",
+    title: "Some title",
+    description: "Some desc",
     image: "",
-    price: 0
+    largeImage: "",
+    price: 1000
   };
-
   handleChange = e => {
     const { name, type, value } = e.target;
     const val = type === "number" ? parseFloat(value) : value;
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    console.log("uploading file...");
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/wesbostutorial/image/upload",
+      {
+        method: "POST",
+        body: data
+      }
+    );
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
-        {(createItem, { loading, error }) => {
-          return (
-            <Form
-              onSubmit={async e => {
-                e.preventDefault();
-                const res = await createItem();
-                console.log(res);
-                Router.push({
-                  pathname: "/item",
-                  query: { id: res.data.createItem.id }
-                });
-              }}
-            >
-              <ErrorMessage error={error} />
-              <fieldset disabled={loading} aria-busy={loading}>
-                <label htmlFor="title">
-                  Title
-                  <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    placeholder="Title"
-                    required
-                    value={this.state.title}
-                    onChange={this.handleChange}
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={async e => {
+              // Stop the form from submitting
+              e.preventDefault();
+              // call the mutation
+              const res = await createItem();
+              // change them to the single item page
+              console.log(res);
+              Router.push({
+                pathname: "/item",
+                query: { id: res.data.createItem.id }
+              });
+            }}
+          >
+            <Error error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    width="200"
+                    src={this.state.image}
+                    alt="Upload Preview"
                   />
-                </label>
+                )}
+              </label>
 
-                <label htmlFor="price">
-                  Price
-                  <input
-                    type="number"
-                    id="price"
-                    name="price"
-                    placeholder="Price"
-                    required
-                    value={this.state.price}
-                    onChange={this.handleChange}
-                  />
-                </label>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </label>
 
-                <label htmlFor="description">
-                  Description
-                  <textarea
-                    type="text"
-                    id="description"
-                    name="description"
-                    placeholder="Description"
-                    required
-                    value={this.state.description}
-                    onChange={this.handleChange}
-                  />
-                </label>
-                <button type="submit">Submit</button>
-              </fieldset>
-            </Form>
-          );
-        }}
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter A Description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
       </Mutation>
     );
   }
 }
 
+export default CreateItem;
 export { CREATE_ITEM_MUTATION };
